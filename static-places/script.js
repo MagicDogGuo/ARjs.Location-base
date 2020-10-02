e8462562c17333d4eb3f175e567c09e94f87952c
z0935323866 <z0935323866@gmail.com>
2020-10-02 21:44:42 +0800

    dramLocal

--- static-places/script.js
+++ static-places/script.js
@@ -13,7 +13,7 @@ function staticLoadPlaces() {
             name: 'Pokèmon',
             location: {
                 // decomment the following and add coordinates:
-                 lat: 30.044250,
+                 lat: 25.044250,
                  lng: 121.571243,
             },
         },
